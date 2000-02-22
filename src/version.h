b911580e50ff8b42ffc2b69329069e2e51a49a30
mertz <mertz@b9a2a475-3c1e-4ae0-83c4-c96c2d81b069>
2000-02-22 18:36:05 +0000

    maj licence LGPL et description
    git-svn-id: http://svn.tls.cena.fr/svn/ivy/ivy-c/trunk@432 b9a2a475-3c1e-4ae0-83c4-c96c2d81b069

--- src/version.h
+++ src/version.h
@@ -1,7 +1,7 @@
 /*
  *      Ivy, C interface
  * 
- *	Copyright (C) 1997-1999
+ *	Copyright (C) 1997-2000
  *	Centre d'Études de la Navigation Aérienne
  *
  *      Authors: François-Régis Colin <colin@cenatoulouse.dgac.fr>
@@ -10,16 +10,18 @@
  *	$Id$
  *
  *	This program is free software; you can redistribute it and/or
- *      modify it under the terms of the GNU General Public License
+ *      modify it under the terms of the GNU LGPL Library General Public License
  *      as published by the Free Software Foundation; either version 2
  *      of the License, or (at your option) any later version.
- *      
+ *      On Debian systems LGPL can be found in the file /usr/doc/copyright/LGPL
+ *      Or look at http://www.gnu.org/copyleft/lgpl.html
+ *
  *      This program is distributed in the hope that it will be useful,
  *      but WITHOUT ANY WARRANTY; without even the implied warranty of
  *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *      GNU General Public License for more details.
  *      
- *      You should have received a copy of the GNU General Public License
+ *      You should have received a copy of the GNU Library General Public License
  *      along with this program; if not, write to the Free Software
  *      Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
